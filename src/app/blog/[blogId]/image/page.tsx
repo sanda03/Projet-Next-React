f74b49ffe008da8be8cf67ae4fd392709770b774
sanda03 <hei.sanda.2@gmail.com>
2024-03-10 16:17:29 +0300

    feat: finish

---
+++ src/app/blog/[blogId]/image/page.tsx
@@ -0,0 +1,22 @@
+import { redirect } from "next/navigation";
+
+import { Blog, BlogMenuList, ViewType } from "@/common/components/blogs";
+import blogs from "@/common/utils/contants";
+
+export default function ViewImageBlog({ params: { blogId } }: { params: { blogId: string } }) {
+  const currentBlog = blogs.find(blog => blog.id === blogId);
+
+  if (!currentBlog) {
+    redirect("/404");
+  }
+
+  return (
+    <div 
+      className="flex ml-[300px] p-5 justify-center"
+      style={{width: "calc(100% - 300px)"}}
+    >
+      <BlogMenuList />
+      <Blog blog={currentBlog} view={ViewType.SHOW_IMAGE} />
+    </div>
+  )
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,10 @@
-import { Blog, ImageView } from "@/common/components/blogs"
-import blogs from "@/common/utils/contants"
+import { Blog, ViewType } from "@/common/components/blogs";
+import blogs from "@/common/utils/contants";
 
 export default function Home() {
   return (
-    <main className="w-full py-5 bg-[#f3f3f3]">
-      {blogs.map(blog => <Blog key={blog.id} blog={blog} imageView={ImageView.NONE} />)}
+    <main className="w-full py-5">
+      {blogs.map(blog => <Blog key={blog.id} blog={blog} view={ViewType.LIST} />)}
     </main>
   )
 }
